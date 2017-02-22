efd303dbf6ebcd60074fdf5c4834cb4d18f3394a
Ilya Pekelny <pekelny@gmail.com>
2017-02-22 17:09:43 +0200

    Entropy

--- model.py
+++ model.py
@@ -23,39 +23,36 @@ def __one_hot_shot(num):
 
 def tag_frames(mfccs, centroids_num):
     kmeans = KMeans(n_clusters=centroids_num, random_state=0).fit(mfccs)
-    return np.array(map(__one_hot_shot(centroids_num), kmeans.labels_))
+    one_hot_shot = __one_hot_shot(centroids_num)
+    return np.array([one_hot_shot(i) for i in kmeans.labels_])
 
-
-def make_consistent_samples(labels, track_duration):
+def make_consistent_samples(labels, track_duration, optimal_entropy):
     """Determine continuous consistent intervals of audio with the same tag.
 
     Could be needed for manual analysis."""
-    track_duration = float(track_duration)
-    frame_duration = track_duration / labels.shape[0]
-
-    samples = []
-    curr_sample = dict(label=None, start=None, end=None)
-    for i, v in enumerate(labels):
-        if curr_sample["label"] is None:
-            curr_sample["label"] = v
-
-        if v == curr_sample["label"]:
-            if curr_sample["start"] is None:
-                curr_sample["start"] = (i-1) * frame_duration if i-1 > 0 else 0
-                curr_sample["end"] = i * frame_duration
-            else:
-                curr_sample["end"] = i * frame_duration
+    sample = np.argmax(labels, 1)
+    divide_idxs = [(0, len(sample)-1)]
+    consistent = []
+    while divide_idxs:
+        idxs = divide_idxs.pop()
+        curr_sample = sample[idxs[0]: idxs[1]]
+        counts = np.bincount(curr_sample)
+        non_zero = counts[np.nonzero(counts)]
+        prob = np.divide(non_zero, len(curr_sample))
+        log2prod = np.vectorize(lambda x: x * np.log2(x))
+        entropy = -sum(log2prod(prob))
+        if entropy > optimal_entropy:
+            median = ((idxs[1] - idxs[0]) // 2) + idxs[0]
+            divide_idxs.append((idxs[0], median))
+            divide_idxs.append((median, idxs[1]))
         else:
-            samples.append((curr_sample["start"], curr_sample["end"]))
-            curr_sample = dict(label=None, start=None, end=None)
-    return samples
+            consistent.append(idxs)
+    return consistent
 
 
 def build_model(output_dim, input_dim=None, input_shape=None):
     """Build keras model"""
     model = Sequential()
-    model.add(Dense(13, input_dim=13, init="normal", activation="relu"))
-    model.add(Dense(13, input_dim=13, init="normal", activation="relu"))
     model.add(Dense(13, input_dim=13, init="normal", activation="relu"))
     model.add(Dense(4, init="normal", activation="softmax"))
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
@@ -85,9 +82,10 @@ def main(train_track, prediction_track, instruments_num, test_track=None):
         print(model.evaluate(test_mfccs, labels, batch_size=32))
     predict_mfccs = audio_to_mfcc(prediction_track)
     prediction = model.predict(predict_mfccs, batch_size=32)
-    return prediction
+    consistent_samples = make_consistent_samples(prediction, 0, 0.4)
+    print(consistent_samples)
 
 if __name__ == "__main__":
     train_track, prediction_track, instruments_num = sys.argv[1:]
     test_track = sys.argv[4] if len(sys.argv) == 5 else None
-    print(main(train_track, prediction_track, int(instruments_num), test_track))
+    main(train_track, prediction_track, int(instruments_num), test_track)
